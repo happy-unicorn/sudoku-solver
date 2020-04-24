c383892578625245bb8b90e30d5516593cfd0401
Michael Chernomazov <michael23black@yandex.ru>
2020-04-24 17:07:58 +0300

    Added Flask app

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,6 +1,12 @@
+from flask import request, render_template, redirect, url_for
 from app.core import core_bp
 
 
-@core_bp.route('/')
+@core_bp.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello, world!!!'
+    if request.method == 'GET':
+        return render_template('pages/index.html')
+    if request.method == 'POST':
+        from random import choice
+        array = [[choice(list(range(1, 10))) for _ in range(9)] for _ in range(9)]
+        return render_template('pages/answer.html', grid=array)
